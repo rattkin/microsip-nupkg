b68bf4b9667fd54b8c7e17549bb6e1b2e727f10b
Jan Müller <muller.jan@gmail.com>
2019-03-25 09:27:06 +0100

    MicroSIP-3.19.14.exe

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.microsip.org/downloads/?file=MicroSIP-3.19.12.exe'
+$url        = 'https://www.microsip.org/downloads/?file=MicroSIP-3.19.14.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'microsip*'
-  checksum      = 'e1aef944d04a451e94044622d224709c1a58de514a07898074af25a5f44df84e'
+  checksum      = '9e5f6e7288f3ad761253b245a0e66b8ab5f84376eb63b5b9802e2355a435c1d1'
   checksumType  = 'sha256'
   silentArgs   = '/S'
   validExitCodes= @(0)
